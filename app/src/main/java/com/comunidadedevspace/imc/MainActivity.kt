3504e3b4b2f98122b50e853c2bfc8e3390c17402
Marcos Vinícius Vieira <marcosv_vieira@hotmail.com>
2024-06-15 12:27:21 -0300

    Clean code

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -12,12 +12,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Recuperar os componenetes EditText
-        // Criar uma váriavél e associar o componente de UI <TextInput>
-        // Recuperar o botão da tela
-        // Colocar ação no botão setOnClickListener
-        // Recuperar o texto digitado no edt peso
-
         val edtPeso = findViewById<TextInputEditText>(R.id.edt_peso)
         val edtAltura = findViewById<TextInputEditText>(R.id.edt_altura)
         val btnCalcular = findViewById<Button>(R.id.btn_calcular)
@@ -28,7 +22,6 @@ class MainActivity : AppCompatActivity() {
             val alturaStr: String = edtAltura.text.toString()
 
             if (pesoStr == "") {
-                //Mostrar mensagem para usuário
                 Snackbar.make(
                     edtPeso,
                     "Por favor, digite um número válido para peso. Ex: 70.5",
@@ -48,21 +41,10 @@ class MainActivity : AppCompatActivity() {
                 val alturaQ2 = altura * altura
                 val resultado = peso / alturaQ2
 
-                // Navegar para próxima tela
-                // Criar o Layout da próxima tela
-                // Passar dados (resultado) para próxima tela
-
-                // Para chamar a próxima tela, precisamos usar o Intent
-                // Intent é uma classe do próprio android e serve para declarar a inteção de fazer algo no sistema operacional android
-                // Intent explicita e Intent Implicita
-
                 val intent = Intent(this, ResultActivity::class.java)
                 intent.putExtra("KEY_RESULT_IMC", resultado)
                 startActivity(intent)
 
-                // Cores
-                //EditText Background + Icone
-                // Gradiente + icone + titulo + descricrição
             }
         }
 
